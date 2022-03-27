11e78d9e545366d98a5e65af84bccba0f3bf63cf
VuHuong511 <84625353+VuHuong511@users.noreply.github.com>
2022-03-27 10:46:21 +0700

    a

---
+++ Controllers/HelpScreenController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HelpScreenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Question1()
+        {
+            return View();
+        }
+        public IActionResult Question2()
+        {
+            return View();
+        }
+        public IActionResult Question3()
+        {
+            return View();
+        }
+        public IActionResult Question4()
+        {
+            return View();
+        }
+        public IActionResult Question5()
+        {
+            return View();
+        }
+        public IActionResult Question6()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        [DataType(DataType.MultilineText)]
+        public string? Description { get; set; }
+
+        [Display(Name = "Create Date")]
+        public DateTime CreatDate { get; set; } = DateTime.UtcNow;
+    }
+}
